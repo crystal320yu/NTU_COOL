99effc19f09eec3099e19691051f1bfbf9a027ff
crystal320yu <b04701116@ntu.edu.tw>
2018-09-20 21:14:40 +0800

    update practice

--- week_1/course_1/practice_1.R
+++ week_1/course_1/practice_1.R
@@ -10,8 +10,9 @@ my.height.cm <-160
   my.height.m <-my.height.cm/100 
   
   # Create my.bmi with BMI(Body Mass Index) formula
+  
   my.bmi <-my.weight.kg/((my.height.m)**2)
-  {
+  
   
   # Use if-else to print matched information
   # Reference: http://www.tpech.gov.taipei/ct.asp?xItem=1794336&CtNode=30678&mp=109171
@@ -19,23 +20,21 @@ my.height.cm <-160
   if (my.bmi >= 35) {
     print(paste("Your bmi: ", my.bmi))
     print("重度肥胖!")
-    }
-  else if ( 30<=my.bmi <35 ) {
+  }else if ( my.bmi>= 30) {
     print(paste("Your bmi: ", my.bmi))
     print("中度肥胖!")
-    }
-  else if ( 27<=my.bmi<30 ) {
+  }else if (my.bmi>=27 ) {
     print(paste("Your bmi: ", my.bmi))
     print("輕度肥胖!")
-    }
-  else if ( 24<=my.bmi<27 ) {
+  }else if (my.bmi>=24 ) {
     print(paste("Your bmi: ", my.bmi))
     print("過重!")
-    }
-  else if ( 18.5<=my.bmi<24 ) {
+  }else if ( my.bmi>=18.5 ) {
     print(paste("Your bmi: ", my.bmi))
     print("正常範圍!")
-    }
+  }else{
+    print(paste("Your bmi: ",my.bmi))
+    print("過輕!")
   }
   
   
